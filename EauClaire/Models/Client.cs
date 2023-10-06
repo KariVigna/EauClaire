7cd4b1b67b180c3768b1b34441744682760957d3
Kari Vigna <karivigna24@gmail.com>
2023-10-05 20:27:45 -0700

    database connection established, home controller and home view index

---
+++ EauClaire/Models/Client.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+using MySqlConnector;
+
+namespace EauClaire.Models
+{
+  public class Client
+  {
+    public int ClientId { get; set; }
+    public string Name { get; set; }
+    public int StylistId { get; set; }
+    public Stylist Stylist { get; set; }
+  }
+}
---
+++ EauClaire/Models/Stylist.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+using MySqlConnector;
+
+namespace EauClaire.Models
+{
+  public class Stylist
+  {
+    public int StylistId { get; set; }
+    public string Name { get; set; }
+  }
+}
